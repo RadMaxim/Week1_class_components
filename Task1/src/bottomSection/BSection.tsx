f79172bf7ad7f38ca1e537ffa573ad57b30cfbde
Maksim <radchuk.maksime@gmail.com>
2024-07-07 20:12:23 +0300

    task1
    developing a project in react using class components

---
+++ Task1/src/bottomSection/BSection.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Card from '../Card/Card';
+import './BSection.css';
+import { State, ElementType } from '../All_Interface/BottomSection';
+class BSection extends React.PureComponent {
+  state: State = {
+    all_elem: [],
+    filter: [],
+    input_value: null,
+  };
+  constructor(props: State) {
+    super(props);
+  }
+  componentDidMount() {
+    const all_elem: ElementType[] = JSON.parse(
+      localStorage.getItem('allPeople') || '[]'
+    );
+    const filter: ElementType[] =
+      JSON.parse(localStorage.getItem('filter_array')) || '[]';
+    const input_value: string = localStorage.getItem('input_value') || '';
+    this.setState({
+      all_elem: all_elem,
+      filter: filter,
+      input_value: input_value,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <main className="main_content">
+          <div className="main_container">
+            {this.state.input_value == null
+              ? this.state.all_elem.map((el: ElementType) => {
+                  return (
+                    <Card
+                      key={el.name}
+                      names={el.name}
+                      homeworld={el.homeworld}
+                      url={el.url}
+                    />
+                  );
+                })
+              : this.state.filter.map((el: ElementType) => {
+                  return (
+                    <Card
+                      key={el.name}
+                      names={el.name}
+                      homeworld={el.homeworld}
+                      url={el.url}
+                    />
+                  );
+                })}
+          </div>
+        </main>
+      </>
+    );
+  }
+}
+
+export default BSection;
